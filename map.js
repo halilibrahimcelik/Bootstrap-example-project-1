5b30a3b5e7310e9457c79db19911a337704b3b90
halilibrahimcelik <you@example.com>
2022-05-26 14:22:23 +0300

    editing minor JS code.

--- map.js
+++ map.js
@@ -4,8 +4,12 @@ class Map {
       lat: 39.897174,
       lng: 32.771146,
     };
+    this.mybutton = document.getElementById("back-to-top");
 
     this.render();
+    this.backToTop();
+    this.scrollToTop();
+    this.clickHandler();
   }
 
   render() {
@@ -24,27 +28,24 @@ class Map {
       map: map,
     });
   }
-}
+  scrollToTop() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      this.mybutton.style.display = "block";
+    } else {
+      this.mybutton.style.display = "none";
+    }
+  }
+  backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 
-new Map();
-
-const mybutton = document.getElementById("back-to-top");
-
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollToTop() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+  clickHandler() {
+    this.mybutton.addEventListener("click", this.backToTop);
   }
 }
 
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+new Map();
